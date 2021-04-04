dad6e57691c7d5abc1b7520feaa1b43ed5610d7f
liamH47 <liamhanafee@gmail.com>
2021-04-04 18:39:47 -0400

    learning v-bind and v-for

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 let app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks',
-        description: 'Cozy things that go on feet'
+        product: 'Custom Print-on-Demand T-shirts',
+        image: './img/angersonShirts/dog-tee.jpeg',
+        link: 'https://www.instagram.com/wet_tobacs/?hl=en',
+        onSale: true,
+        details: ['Custom Print-on-Demand Shirt', 'Available in short or long sleeve', '100% Cotton', 'Hand-made'],
+        variants: [
+            {
+                variantId: 1,
+                variantName: 'Short Sleeve'
+            },
+            {
+                variantId: 2,
+                variantName: 'Long Sleeve'
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function(){
+            this.cart += 1
+        }
     }
 })
