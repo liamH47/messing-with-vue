6a88314f14fc633b2f514cc7673c24d0fd90fc61
liamH47 <liamhanafee@gmail.com>
2021-04-04 21:29:23 -0400

    color box slightly weird still, but enjoying the vue so far

--- main.js
+++ main.js
@@ -9,18 +9,43 @@ let app = new Vue({
         variants: [
             {
                 variantId: 1,
-                variantName: 'Short Sleeve'
+                variantName: 'Dog',
+                variantImage: './img/angersonShirts/dog-tee.jpeg',
+                backgroundImage: './img/angersonShirts/dog-tee.jpeg'
             },
             {
                 variantId: 2,
-                variantName: 'Long Sleeve'
+                variantName: 'Doom',
+                variantImage: './img/angersonShirts/doom-tee.jpeg',
+                backgroundImage: './img/angersonShirts/doom-tee.jpeg'
+
+            },
+            {
+                variantId: 3,
+                variantName: 'Succubus',
+                variantImage: './img/angersonShirts/succubus-tee.jpeg',
+                backgroundImage: './img/angersonShirts/succubus-tee.jpeg'
+
+            },
+            {
+                variantId: 4,
+                variantName: 'Vile Tribe',
+                variantImage: './img/angersonShirts/vile-tribe-tee.jpeg',
+                backgroundImage: './img/angersonShirts/vile-tribe-tee.jpeg'
+
             }
         ],
         cart: 0
     },
     methods: {
-        addToCart: function(){
-            this.cart += 1
+        addToCart(){
+            this.cart++;
+        },
+        removeFromCart(){
+            this.cart--;
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage;
         }
     }
 })
